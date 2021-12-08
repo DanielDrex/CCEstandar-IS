aacccbc49c8e98b59dbb44536f993c106f8be59c
DanielDrex <daniel23castrejon@gmail.com>
2021-12-08 16:55:11 -0600

    Estilo apartados.edit

--- resources/views/apartados/edit.blade.php
+++ resources/views/apartados/edit.blade.php
@@ -1,34 +1,50 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<script type="text/javascript" src="http://code.jquery.com/jquery-3.5.1.js"></script>
-	<title>Editar apartado</title>
+	<title> Editar apartado </title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<link href="{{asset('estilo/css/base.css')}}" rel="stylesheet" />
+	<link href="{{asset('estilo/css/base_form.css')}}" rel="stylesheet" />
 </head>
 
 <body>
+	<div class="jumbotron-jumbotron-fluid">
+		<div class="container">
+			<h1 class="display-4">Control de calidad de estandares</h1>
+			<p class="lead">Edicion de apartados </p>
+		</div>
+		<section class="img-intro">
+			<figure>
+				<img src="https://www.itmastersmag.com/wp-content/uploads/2021/01/shutterstock_1078387013-2048x1024.jpg" alt="CCE">
+			</figure>
+		</section>
+		<div>
+			{!! Form::open(['method'=>'PATCH','url'=>'/apartados/'.$apartado->id]) !!}
 
-
-	<h1>Editar un apartado</h1>
-
-
-	{!! Form::open(['method'=>'PATCH','url'=>'/apartados/'.$apartado->id]) !!}
-
-	{!! Form::label ('id_estandar','Estandar') !!}
-	{!! Form::select ('id_estandar', $estandares->pluck('nombre','id')->all(),$apartado->id_estandar) !!}
-
-	<br>
-	<br>
-
-	{!! Form::label ('nombre','Nombre') !!}
-	{!! Form::text ('nombre',$apartado->nombre,['placeholder'=>'Nombre','required']) !!}
-
-	<br>
-	<br>
-
-	{!! Form::submit('Guardar editado de apartado') !!}
-	{!! Form::close() !!}
-
+			<div class="opciones">
+				<span class="input-group-text" id="basic-addon1">Estandar</span> &nbsp; &nbsp;
+				{!! Form::select ('id_estandar', $estandares->pluck('nombre','id')->all(),$apartado->id_estandar) !!}
+			</div>
+			{!! Form::open(['method'=>'PATCH','url'=>'/apartados/'.$apartado->id]) !!}
+			<div class="opciones">
+				<div class="input-group mb-3">
+					<span class="input-group-text" id="basic-addon1">Nombre</span>
+					<input type="text" class="form-control" placeholder="Nombre del estandar" aria-label="Username" aria-describedby="basic-addon1" name="nombre" id="validationCustom01" required value="{!! $apartado->nombre!!}">
+				</div>
+			</div>
+		</div>
+		<div class="opcnuevas">
+			<input class="btn btn-primary" type="submit" value="Guardar editado">
+			{!! Form::close() !!}
+			<div class="input">
+				<a class="btn btn-primary" href="{!! '/apartados' !!}" role="button">Cancelar</a>
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
