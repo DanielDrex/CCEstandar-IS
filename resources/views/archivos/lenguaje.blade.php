5876adf4fbd8184b2bab24095c4c0cfc47cc9266
DanielDrex <daniel23castrejon@gmail.com>
2021-12-08 20:22:33 -0600

    Estilo Archivos.lenguajes

--- resources/views/archivos/lenguaje.blade.php
+++ resources/views/archivos/lenguaje.blade.php
@@ -1,28 +1,48 @@
+<!DOCTYPE html>
+<html>
+
 <head>
 	<meta charset="utf-8">
 	<title>Lenguaje compatible</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<link href="{{asset('estilo/css/base.css')}}" rel="stylesheet" />
 </head>
+
 <body>
-	<h1>Lenguaje del archivo:</h1>
-	<h2>{!! $archivo->ruta !!}</h2>
-	<table>
-		<tr>
-			<th>Lenguaje</th>
-			<th>Extension</th>
-		</tr>
-		@foreach($lenguajes as $lenguaje)
-		<tr>
-			<td>{!! $lenguaje->nombre !!}</td>
-			<td>{!! $lenguaje->extension !!}</td>
-			<td style="background-color:white;">		
-				<a href="{!! asset('select_estandar/'.$lenguaje->id.'/'.$archivo->id) !!}">ver estandares</a>
-			</td>
-		</tr>
-		@endforeach
-</table>
-<br>
+	<div class="jumbotron-jumbotron-fluid">
+		<div class="container">
+			<h1 class="display-4">Control de calidad de estandares</h1>
+			<p class="lead">{!! $archivo->ruta !!} </p>
+		</div>
+		<section class="img-intro">
+			<figure>
+				<img src="https://www.itmastersmag.com/wp-content/uploads/2021/01/shutterstock_1078387013-2048x1024.jpg" alt="CCE">
+			</figure>
+		</section>
+		<div class="listado">
+			<table>
+				<tr>
 
-<a href="{!! asset('home') !!}">Regresar al home</a>
-
-
+					<th>Lenguaje</th>
+					<th>Extension</th>
+					<th>Opciones</th>
+				</tr>
+				@foreach($lenguajes as $lenguaje)
+				<tr>
+					<td>{!! $lenguaje->nombre !!}</td>
+					<td>{!! $lenguaje->extension !!}</td>
+					<td class="opc">
+						<a class="btn btn-primary" href="{!! asset('select_estandar/'.$lenguaje->id.'/'.$archivo->id) !!}" role="button">Ver estandares</a>
+					</td>
+				</tr>
+				@endforeach
+			</table>
+		</div>
+		<div class="opcnuevas">
+			<a class="btn btn-primary" href="{!! asset('home') !!}" role="button">Regresar al home</a>
+		</div>
+	</div>
 </body>
+
+</html>
