4a27b9d7968bfb401d6850a157a30813f5837085
DanielDrex <daniel23castrejon@gmail.com>
2021-12-08 20:32:24 -0600

    Estilo Archivo.estandar

--- resources/views/archivos/estandar.blade.php
+++ resources/views/archivos/estandar.blade.php
@@ -1,27 +1,48 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<title>Estandares</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<link href="{{asset('estilo/css/base.css')}}" rel="stylesheet" />
 </head>
-<body>
-	<h1>Selecciona un estandar para evaluar en el lenguaje:</h1>
-	<h2>{!! $lenguaje->nombre !!}</h2>
-	<table>
-		<tr>
-			<th>Estandar</th>
-		</tr>
-		@foreach($estandares as $estandar)
-		<tr>
-			<td>{!! $estandar->nombre !!}</td>
-			<td>
-				<a href="{!! asset('evaluar/'.$estandar->id.'/'.$archivo->id) !!}">Seleccionar estandar y evaluar</a>
-			</td>
-		</tr>
-		@endforeach
-</table>
-<br>
-<a href="{!! asset('home') !!}">Regresar al home</a>
 
+<body>
+	<div class="jumbotron-jumbotron-fluid">
+		<div class="container">
+			<h1 class="display-4">Control de calidad de estandares</h1>
+			<p class="lead">Selecciona un estandar para evaluar en el lenguaje: </p>
+			<p class="lead">{!! $lenguaje->nombre !!} </p>
+
+		</div>
+		<section class="img-intro">
+			<figure>
+				<img src="https://www.itmastersmag.com/wp-content/uploads/2021/01/shutterstock_1078387013-2048x1024.jpg" alt="CCE">
+			</figure>
+		</section>
+		<div class="listado">
+			<table>
+				<tr>
+
+					<th>Estandar</th>
+					<th>Opciones</th>
+				</tr>
+				@foreach($estandares as $estandar)
+				<tr>
+					<td>{!! $estandar->nombre !!}</td>
+					<td class="opc">
+						<a class="btn btn-primary" href="{!! asset('evaluar/'.$estandar->id.'/'.$archivo->id) !!}" role="button">Seleccionar estandar y evaluar</a>
+					</td>
+				</tr>
+				@endforeach
+			</table>
+		</div>
+		<div class="opcnuevas">
+			<a class="btn btn-primary" href="{!! asset('home') !!}" role="button">Regresar al home</a>
+		</div>
+	</div>
 </body>
+
 </html>
